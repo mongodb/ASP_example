3e6039cb3e3b126b590fcd4949e186d3b8ae233a
Joe Niemiec <josephxsxn@gmail.com>
2025-06-11 22:26:54 -0400

    Update opscmds.js

--- code_snipplets/opscmds.js
+++ code_snipplets/opscmds.js
@@ -13,3 +13,11 @@ pipeline = sp.name.stats().pipeline
 pipeline[0]["$source"]["config"]["fullDocument"] = "whenAvailable"
 sp.name.modify(pipeline, {resumeFromCheckpoint: false})
 sp.name.start()
+
+
+#mod processor to restart changestream at specific time
+sp.stuff.stop()
+pipeline = sp.stuff.stats().pipeline
+pipeline[0]["$source"]["config"]["startAtOperationTime"] = new Date("2025-06-12T01:00:00.000Z")
+sp.stuff.modify(pipeline, {resumeFromCheckpoint: false})
+sp.stuff.start()
