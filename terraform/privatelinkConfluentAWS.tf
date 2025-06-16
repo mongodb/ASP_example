e593b8392f15a91dd24722fcf19115dfec488d40
Joe Niemiec <josephxsxn@gmail.com>
2025-06-16 11:50:25 -0400

    Update privatelinkConfluentAWS.tf

--- terraform/privatelinkConfluentAWS.tf
+++ terraform/privatelinkConfluentAWS.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas",
-      version = "1.31.0"
+      version = "1.36.0"
     }
     confluent = {
       source  = "confluentinc/confluent"
@@ -12,13 +12,13 @@ terraform {
 }
 
 provider "confluent" {
-  cloud_api_key    = "..."    # USING Cloud resource management access
-  cloud_api_secret = "..." 
+  cloud_api_key    = "....."    # USING 
+  cloud_api_secret = "......" # optionally use CONFLUENT_CLOUD_API_SECRET env var
 }
 
 provider "mongodbatlas" {
-  public_key = "..."
-  private_key  = "..."
+  public_key = "....."
+  private_key  = "....."
 }
 
 resource "confluent_environment" "staging" {
@@ -38,10 +38,16 @@ resource "confluent_network" "private_link" {
   }
 }
 
+data "mongodbatlas_stream_account_details" "account_details" {
+  project_id     = "....."
+  cloud_provider = "aws"
+  region_name    = "US_EAST_1"
+}
+
 resource "confluent_private_link_access" "aws" {
   display_name = "example-private-link-access"
   aws {
-    account = "..." # MONGO AWS ACCOUNT for the specific atlas project and region used https://www.mongodb.com/docs/atlas/reference/api-resources-spec/v2/#tag/Streams/operation/getAccountDetails
+    account = data.mongodbatlas_stream_account_details.account_details.aws_account_id
   }
   environment {
     id = confluent_environment.staging.id
@@ -84,7 +90,7 @@ output "plsubdns" {
 
 
 resource "mongodbatlas_stream_privatelink_endpoint" "test" {
-  project_id          = "..." # Atlas Project ID
+  project_id          = "....."
   dns_domain          = confluent_network.private_link.dns_domain
   provider_name       = "AWS"
   region              = "us-east-1"
@@ -94,7 +100,7 @@ resource "mongodbatlas_stream_privatelink_endpoint" "test" {
 }
 
 data "mongodbatlas_stream_privatelink_endpoint" "singular_datasource" {
-  project_id = "...." # Atlas Project ID
+  project_id = "....."
   id         = mongodbatlas_stream_privatelink_endpoint.test.id
 }
 
@@ -102,70 +108,24 @@ output "interface_endpoint_id" {
   value = data.mongodbatlas_stream_privatelink_endpoint.singular_datasource.interface_endpoint_id
 }
 
-resource "mongodbatlas_stream_instance" "test" {
-    project_id = "..." # Atlas Project ID
-    instance_name = "JSNTFTESTING"
-    data_process_region = {
-        region = "VIRGINIA_USA"
-        cloud_provider = "AWS"
-  }
-  stream_config = {
-    tier = "SP10"
-    }
+# Outputs the Atlas-side AWS account ID (derived from interface endpoint data) which can/should be
+# used as input to configure Confluent-side network access
+output "mongodbatlas_stream_confluent_cloud_ckc_privatelink_aws_account_id" {
+  value = data.mongodbatlas_stream_privatelink_endpoint.singular_datasource.provider_account_id
 }
 
-resource "mongodbatlas_stream_connection" "jsncluster0" {
-    project_id = "..." # Atlas Project ID
-    instance_name = "JSNTFTESTING"
-    connection_name = "jsncluster0conn"
-    type = "Cluster"
-    cluster_name = "jsncluster0"
-    db_role_to_execute = {
-       type = "BUILT_IN"
-       role = "atlasAdmin"
-    }
+output "aws_account_id" {
+  value = data.mongodbatlas_stream_account_details.account_details.aws_account_id
 }
 
-resource "mongodbatlas_stream_connection" "kafkacluster" {
-    project_id = ".." # Atlas Project ID
-    instance_name = "JSNTFTESTING"
-    connection_name = "kafkapl"
-    type = "Kafka"
-    authentication = {
-        mechanism = "PLAIN"
-        username = "..."
-        password = "..."
-    }
-    security = {
-        protocol = "SSL"
-    }
-    bootstrap_servers = "lkc-8jwz05.dom1w421e2g.us-east-1.aws.confluent.cloud:9092"
-
-    networking = {
-      access = {
-        type = "PRIVATE_LINK"
-        connection_id = data.mongodbatlas_stream_privatelink_endpoint.singular_datasource.id
-      }
-    }
+output "cidr_block" {
+  value = data.mongodbatlas_stream_account_details.account_details.cidr_block
 }
 
-#processor file
-/* 
-[
-{"$source" : {"connectionName" : "jsncluster0conn", "db" : "test", "coll" : "pltestdata"}},
-{"$emit" : {"connectionName" : "kafkapl", "topic" : "test"}}
-]
-*/
-
-data "local_file" "processor" {
-  filename = "PATHTOPROCESSORFILE" 
+output "cloud_provider" {
+  value = data.mongodbatlas_stream_account_details.account_details.cloud_provider
 }
 
-resource "mongodbatlas_stream_processor" "stream-processor-change-stream" {
-  project_id = "...." # Atlas Project ID
-  instance_name = "JSNTFTESTING"
-  processor_name = "csstream"
-  pipeline       = data.local_file.processor.content
-  state          = "STARTED"
+output "vpc_id" {
+  value = data.mongodbatlas_stream_account_details.account_details.vpc_id
 }
-
