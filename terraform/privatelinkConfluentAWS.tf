b383b927ce433ebd0ec6241a9f234573f287bf3b
Joe Niemiec <josephxsxn@gmail.com>
2025-06-16 14:04:39 -0400

    Update privatelinkConfluentAWS.tf

--- terraform/privatelinkConfluentAWS.tf
+++ terraform/privatelinkConfluentAWS.tf
@@ -90,7 +90,7 @@ output "plsubdns" {
 
 
 resource "mongodbatlas_stream_privatelink_endpoint" "test" {
-  project_id          = "...."
+  project_id          = "..." # Atlas Project ID
   dns_domain          = confluent_network.private_link.dns_domain
   provider_name       = "AWS"
   region              = "us-east-1"
@@ -100,7 +100,7 @@ resource "mongodbatlas_stream_privatelink_endpoint" "test" {
 }
 
 data "mongodbatlas_stream_privatelink_endpoint" "singular_datasource" {
-  project_id = "...."
+  project_id = "...." # Atlas Project ID
   id         = mongodbatlas_stream_privatelink_endpoint.test.id
 }
 
@@ -108,24 +108,69 @@ output "interface_endpoint_id" {
   value = data.mongodbatlas_stream_privatelink_endpoint.singular_datasource.interface_endpoint_id
 }
 
-# Outputs the Atlas-side AWS account ID (derived from interface endpoint data) which can/should be
-# used as input to configure Confluent-side network access
-output "mongodbatlas_stream_confluent_cloud_ckc_privatelink_aws_account_id" {
-  value = data.mongodbatlas_stream_privatelink_endpoint.singular_datasource.provider_account_id
+resource "mongodbatlas_stream_instance" "test" {
+    project_id = "..." # Atlas Project ID
+    instance_name = "JSNTFTESTING"
+    data_process_region = {
+        region = "VIRGINIA_USA"
+        cloud_provider = "AWS"
+  }
+  stream_config = {
+    tier = "SP10"
+    }
 }
 
-output "aws_account_id" {
-  value = data.mongodbatlas_stream_account_details.account_details.aws_account_id
+resource "mongodbatlas_stream_connection" "jsncluster0" {
+    project_id = "..." # Atlas Project ID
+    instance_name = "JSNTFTESTING"
+    connection_name = "jsncluster0conn"
+    type = "Cluster"
+    cluster_name = "jsncluster0"
+    db_role_to_execute = {
+       type = "BUILT_IN"
+       role = "atlasAdmin"
+    }
 }
 
-output "cidr_block" {
-  value = data.mongodbatlas_stream_account_details.account_details.cidr_block
+resource "mongodbatlas_stream_connection" "kafkacluster" {
+    project_id = ".." # Atlas Project ID
+    instance_name = "JSNTFTESTING"
+    connection_name = "kafkapl"
+    type = "Kafka"
+    authentication = {
+        mechanism = "PLAIN"
+        username = "..."
+        password = "..."
+    }
+    security = {
+        protocol = "SSL"
+    }
+    bootstrap_servers = "lkc-8jwz05.dom1w421e2g.us-east-1.aws.confluent.cloud:9092"
+
+    networking = {
+      access = {
+        type = "PRIVATE_LINK"
+        connection_id = data.mongodbatlas_stream_privatelink_endpoint.singular_datasource.id
+      }
+    }
 }
 
-output "cloud_provider" {
-  value = data.mongodbatlas_stream_account_details.account_details.cloud_provider
+#processor file
+/* 
+[
+{"$source" : {"connectionName" : "jsncluster0conn", "db" : "test", "coll" : "pltestdata"}},
+{"$emit" : {"connectionName" : "kafkapl", "topic" : "test"}}
+]
+*/
+
+data "local_file" "processor" {
+  filename = "/../.." 
 }
 
-output "vpc_id" {
-  value = data.mongodbatlas_stream_account_details.account_details.vpc_id
+resource "mongodbatlas_stream_processor" "stream-processor-change-stream" {
+  project_id = "...." # Atlas Project ID
+  instance_name = "JSNTFTESTING"
+  processor_name = "csstream"
+  pipeline       = data.local_file.processor.content
+  state          = "STARTED"
 }
