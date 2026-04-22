42c6349d19baffc3652b6c75a0b92e6140f2fdf4
Joe Niemiec <josephxsxn@gmail.com>
2026-04-22 09:39:02 -0400

    Update source_sinks.js

--- code_snippets/source_sinks.js
+++ code_snippets/source_sinks.js
@@ -34,28 +34,22 @@ s = {
     }
 
 --Change Stream Source starting in the past of the oplog with startAtOperationTime        
-s = {
-    $source:  {
+    {
+      '$source': {
         connectionName: 'jsncluster0',
+        coll: 'test',
         db: 'test',
-        coll : 'datetest',
-        config : {
-              startAtOperationTime : Timestamp(new Date('2024-01-30T00:00:01Z').getTime()/1000,0),
-            },        
+        config: { startAtOperationTime: ISODate('2026-04-22T13:00:00.000Z') }
+      }
     }
-}
 
 --Change Stream Source starting with OpLog resume token
-s = {
-    $source:  {
-        connectionName: 'jsncluster0',
-        db: 'test',
-        coll : 'datetest',
-        config : {
-            startAfter : new Object({_data : '8265B8F36B000000022B042C0100296E5A100469E03217B2954C6580FCAD5A7D225571463C6F7065726174696F6E54797065003C696E736572740046646F63756D656E744B65790046645F6964006465B8F36BBDD90E425EC09B31000004'}),
-            },        
-    }
-}
+s = {$source : {connectionName : "jsncluster0", 
+                coll : "test", 
+                db : "test",
+                config: { 
+                  startAfter: {_data: '8269E8C9EB000000012B042C0100296E5A1004DCB26606099F464791A91C014F17A562463C6F7065726174696F6E54797065003C696E736572740046646F63756D656E744B65790046645F6964006469E8C9EB34F70B2997F84340000004'}  }
+}}
 
 --Merge to a Atlas Cluster Collection
 merge = {
